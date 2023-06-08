91f87fd98845cbab2bd0355ea0bdc5d673595567
CodeWizardette <131625021+CodeWizardette@users.noreply.github.com>
2023-06-08 03:02:44 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+def generate_passwords(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    passwords = []
+
+    with open("passwords.txt", "w") as file:
+        count = 0
+        while count < 100000:
+            password = ''.join(random.choice(characters) for i in range(length))
+            if password not in passwords:
+                passwords.append(password)
+                file.write(password + "\n")
+
+                count += 1
+                if count % 10000 == 0:
+                    print(f"{count} pass saved.")
+
+    print("Tüm şifreler başarıyla kaydedildi.")
+
+#password length 
+length = int(input("Şifre uzunluğunu girin: "))
+
+# pass export .txt save
+generate_passwords(length)
